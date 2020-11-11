8386cc24535b1d8369d0a341b472b180de49f8bb
skywateryang <50293686+skywateryang@users.noreply.github.com>
2020-11-11 13:01:44 +0800

    reorganize

---
+++ watchlist/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_login import LoginManager
+app = Flask(__name__)
+login_manager = LoginManager(app)  # 实例化扩展类
+login_manager.login_view = 'login'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+app.config['SECRET_KEY'] = 'dev'
+
+db = SQLAlchemy(app)
+
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    from watchlist.models import User
+    user = User.query.first()
+    return dict(user=user)  # 需要返回字典，等同于 return {'user': user}
+
+
+
+@login_manager.user_loader
+def load_user(user_id):  # 创建用户加载回调函数，接受用户 ID 作为参数
+    from watchlist.models import User
+    user = User.query.get(int(user_id))  # 用 ID 作为 User 模型的主键查询对应的用户
+    return user  # 返回用户对象
+
+from watchlist import views, errors, commands
